ca0c3ff5b71be4febd6d9aa0313feae414abb222
Julio Ribeiro <wwwjuliords@gmail.com>
2013-01-04 15:28:23 -0200

    improved result set: now search by prefix also

--- geogle/views.py
+++ geogle/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render_to_response
 from geogle.models import Article
+from string import punctuation
 
 def IndexView(request):
 	query = ""
 	articles = []
 
 	if request.GET.has_key("q") == True and len(request.GET["q"]) > 0:
-		articles = Article.objects.raw("SELECT id, title, abstract, author_id, date FROM geogle_article WHERE search_index @@@ to_tsquery(simples(%s));", [request.GET["q"]])
 		query = request.GET["q"]
+		tsquery = ''
+		for word in query.strip(punctuation).split(' '):
+			if len(word) > 0:
+				if len(tsquery) == 0:
+					tsquery = (word+':*')
+				else:
+					tsquery = tsquery + ' | ' + (word+':*')
+
+		articles = Article.objects.raw("SELECT id, title, abstract, author_id, date FROM geogle_article WHERE search_index @@@ to_tsquery(simples(%s));", [tsquery])
 
 	return render_to_response('search.html', {"query":query, "articles":articles})
 
