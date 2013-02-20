9727da79c797c93b284ccd1e48148b36b89177f7
Luiza Silva <luiza@royalsflush.net>
2013-02-20 19:49:38 -0300

    no success with outlets; they seem to not have internal logic

--- static/js-dev/app.js
+++ static/js-dev/app.js
@@ -1,15 +1,30 @@
 App = Ember.Application.create();
 
 App.ApplicationView = Ember.View.extend({
-  templateName: 'application'
+	templateName: 'big'
 });
 App.ApplicationController = Ember.Controller.extend();
 
+App.SearchView = Ember.View.extend({
+	templateName: 'search'
+});
+App.SearchController = Ember.Controller.extend();
+
 
 App.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-    index: Ember.Route.extend({
-      route: '/'
-    })
-  })
+	root: Ember.Route.extend({
+		index: Ember.Route.extend({
+			route: '/',
+			redirectsTo: 'search'
+		}),
+
+		search: Ember.Route.extend({
+			route: '/search',
+			connectOutlets: function(router) {
+				var tmp=router.get('applicationController');
+				console.log(tmp);
+				tmp.connectOutlet('search');
+			}
+		})
+	})
 });
