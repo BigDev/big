a7c283ab81c9a8e75f825ecefa5cafe95319e26a
Luiza Silva <luiza@royalsflush.net>
2013-04-23 22:21:51 -0300

    search and advanced search look like shit, but are working

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,9 @@
-App = Ember.Application.create();
+Big = Ember.Application.create();
+
+Big.Router.map(function() {
+	this.resource('search');
+	this.resource('adv-search');
+	this.resource('articles', function() {
+		this.resource('article', {path: ':article_id'});
+	});
+});
