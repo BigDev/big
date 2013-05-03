d3abef69b7a3b39c8804bcea274ecf094f7642e6
Luiza Silva <luiza@royalsflush.net>
2013-05-03 16:18:09 -0300

    added bootstrap and redone the view architecture for easier handling of a view for search templates

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,16 @@
 Big = Ember.Application.create();
 
 Big.Router.map(function() {
-	this.resource('search');
-	this.resource('adv-search');
+	this.resource('search', function() {
+		this.resource('adv');
+	});
 	this.resource('articles', function() {
 		this.resource('article', {path: ':article_id'});
 	});
+});
+
+Big.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+		this.transitionTo('search/index');
+	}
 });
