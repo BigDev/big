7fe7f931cfd0ca17142219683a7ce086ea442713
Luiza Silva <luiza@royalsflush.net>
2013-05-03 16:41:00 -0300

    started working on some CSS, transition between search and adv works good, but layout is crap... oh well, thats for later

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ Big = Ember.Application.create();
 
 Big.Router.map(function() {
 	this.resource('search', function() {
-		this.resource('adv');
+		this.route('adv');
 	});
 	this.resource('articles', function() {
 		this.resource('article', {path: ':article_id'});
@@ -11,6 +11,6 @@ Big.Router.map(function() {
 
 Big.IndexRoute = Ember.Route.extend({
 	redirect: function() {
-		this.transitionTo('search/index');
+		this.transitionTo('search');
 	}
 });
