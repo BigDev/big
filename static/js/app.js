b2870e2230491de1800d048a501a61e33e32dafa
Luiza Silva <luiza@royalsflush.net>
2013-05-06 03:45:17 -0300

    article list is working with fixture model

--- static/js/app.js
+++ static/js/app.js
@@ -4,14 +4,25 @@ Big.Router.map(function() {
 	this.resource('search', function() {
 		this.route('adv');
 	});
-	this.resource('articles', function() {
-		this.resource('article', {path: ':article_id'});
-	});
+	this.resource('articles');
+	this.resource('article', {path: ':article_id'});
 });
 
 Big.IndexRoute = Ember.Route.extend({
 	redirect: function() {
 		this.transitionTo('search');
+	}
+});
+
+Big.ArticlesRoute = Ember.Route.extend({
+	renderTemplate: function() {
+		this.render();
+		this.render('upper-search', {into: 'articles', outlet: 'upperSearch'});
+		this.render('article-list', {into: 'articles', outlet: 'articleList'});
+	},
+	model: function() {
+		console.log(Big.Article.find());
+		return Big.Article.find();
 	}
 });
 
@@ -29,6 +40,47 @@ Big.SearchView = Ember.View.extend({
 		var wh = $(window).height();
 		console.log(wh);
 
+		console.log(this.$().find('#search-div').height());
 		console.log(this.$());
 	}
+});
+
+Big.Store = DS.Store.extend({
+	revision: 12,
+	adapter: 'DS.FixtureAdapter'
+});
+
+Big.Article = DS.Model.extend({
+	title: DS.attr('string'),
+	author: DS.attr('string'),
+	abstrac: DS.attr('string')
+});
+
+Big.Article.FIXTURES = [{
+	'id': 1,
+	'title': 'Article1',
+	'author': 'Author1',
+	'abstrac': 'This is an abstract act act act'
+},
+{
+	'id': 2,
+	'title': 'Article1',
+	'author': 'Author1',
+	'abstrac': 'This is an abstract act act act'
+},
+{
+	'id': 3,
+	'title': 'Article1',
+	'author': 'Author1',
+	'abstrac': 'This is an abstract act act act'
+},
+{
+	'id': 4,
+	'title': 'Article1',
+	'author': 'Author1',
+	'abstrac': 'This is an abstract act act act'
+}]
+
+Big.ArticlesController = Ember.Controller.extend({
+	
 });
