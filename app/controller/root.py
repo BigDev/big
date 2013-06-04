e51a81680a0db8858b0a06956275287fe9d3d00d
Julio Ribeiro <wwwjuliords@gmail.com>
2013-06-04 08:42:01 +0000

    api layer added

--- app/controller/root.py
+++ app/controller/root.py
@@ -4,12 +4,16 @@ from app.controller.error import Error
 from app.controller.article import Article
 from app.controller.search import Search
 
+class Api(object):
+	exposed = True
+	search = Search()
+
 class Root(object):
 	exposed = True
 
 	error = Error()
 	article = Article()
-	search = Search()
+	api = Api()
 
         @cherrypy.tools.mako(filename="index.html")
 	def GET(self):
